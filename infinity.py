6ed448eebc001923736cd9d12ca68a35be270da4
burumsak <89971436+burumsak@users.noreply.github.com>
2021-12-07 16:18:06 +0530

    Update infinity.py

--- infinity.py
+++ infinity.py
@@ -22,9 +22,6 @@ from nltk.corpus import stopwords
 from sklearn.neighbors import NearestNeighbors
 from sklearn.feature_extraction.text import TfidfVectorizer
 from nltk.stem.snowball import SnowballStemmer
-import xlrd
-
-
 
 
 def main():
@@ -36,70 +33,69 @@ def main():
     """
     st.markdown(html_temp,unsafe_allow_html=True)
 
-    
-
-
 st.markdown('##')
 
-okc = pd.read_excel("Copy of User Details_Faf.xlsx")
+okc = pd.read_excel("D:/5th Sem/Project/Copy of User Details_Faf.xlsx")
 st.title("Home")
 
 if __name__=='__main__':
     main() 
     
 st.markdown('##')  
-    
-if st.checkbox("Show data"):
+
+with st.beta_expander("Show data"):
     st.write(okc)
+st.write("") 
+st.markdown('##')
   
-st.markdown('##')  
- 
-   
 '''
-
-Fill These List Of Questions To Get Your Match
-
+Answer these questions to find new people to interact with!
 '''   
 st.markdown('##')
 
-     
-     
 
 okc1 = pd.DataFrame() 
 
-#def user_input_features():
-name = st.text_input("Your Name Please","Type..") 
-age = st.number_input("Age",18,79) 
-status = st.selectbox(" Status", ["Single","in a relationship","Unknown"]) 
-gender = st.selectbox("Gender", ["Female","Male"]) 
-orientation = st.selectbox("Orientation", ["Straight","Bisexual","Gay"]) 
-body_type = st.selectbox("body_Type", ["Fit","Average","Curvy","Thin","Overweight","Rather not say"]) 
-education = st.selectbox("Education", ["college/university","Masters and above","other","Two-year college","High school","Med / Law school"]) 
-ethnicity= st.selectbox("Ethnicity", ["White","Asian","Hispanic","African American","Mixed","Unknown","others"]) 
-religion = st.selectbox("Religion", ["Agnosticism","Atheism","Christianity","Catholicism","Judaism","Buddhism","Islam","Hinduism","Unknown","others"]) 
-smokes = st.selectbox("Smokes", ["Yes","No"]) 
-drink = st.selectbox("Drink", ["Yes","No"]) 
-diet = st.selectbox("Diet", ["Anything","Vegan","Vegetarian","Halal","Kosher","other"]) 
-speaks = st.text_input("Speaks ","Type..") 
-sign = st.selectbox("Sign", ["Aries","Taurus","Gemini","Cancer","Leo","Virgo","Libra","Scorpion","Sagittarius","Capricorns","Aquarius","Pisces"]) 
-offspring = st.selectbox("Offspring", ["Wants Kids","Does not want kids","Has kid","Unknown"]) 
-drugs = st.selectbox("Drugs", ["Yes","No"]) 
-height = st.number_input("Height \n (In inches)",30,100) 
-income = st.number_input("Income",0,100000) 
-pets = st.selectbox("Pets Preference", ["Likes Cats and Dogs","Dislikes Cats and Dogs","Likes only cats","Likes only Dogs","Unknown"]) 
-job = st.selectbox("Job", ["Office/Professional","Science/Tech","Business Management","Creative"]) 
-essay0 = st.text_input("My Self Summary","Type..") 
-essay1 = st.text_input("What I Am Doing With My Life","Type..") 
-essay2 = st.text_input("I am really good at ","Type..") 
-essay3 = st.text_input(" The First Thing People Usually Notice About Me","Type..") 
-essay4 = st.text_input(" Favourite Books, Movies, Show, Music, Food","Type..") 
-essay5 = st.text_input(" The 6 Things That I Could Never Do Without","Type..") 
-essay6 = st.text_input(" I Spend A Lot Of Time Thinking About","Type") 
-essay7 = st.text_input(" On A Typical Friday Night I Am","Type..") 
-essay8 = st.text_input(" The Most Private Thing I Am Willing To Admit","Type..") 
-essay9 = st.text_input(" You Should Message Me If","Type..") 
+col1, col2 = st.beta_columns(2)
 
-   
+#form for recommendations
+
+with st.form(key='my_form'):
+    col1, col2 = st.beta_columns(2)
+    with col1:
+        name = st.text_input("Name")
+        age = st.number_input("Age",18,79)
+        status = st.selectbox("Relationship Status", ["Single","In a relationship","Unknown"])
+        gender = st.selectbox("Gender", ["Female","Male"])
+        orientation = st.selectbox("Sexual Orientation", ["Straight","Bisexual","Gay"])
+        body_type = st.selectbox("How would you describe your body type?", ["Fit","Average","Curvy","Thin","Overweight","Rather not say"])
+        education = st.selectbox("What level of education have you completed?", ["college/university","Masters and above","other","Two-year college","High school","Med / Law school"])
+        ethnicity= st.selectbox("Which ethnicity do you belong to?", ["White","Asian","Hispanic","African American","Mixed","Unknown","others"])
+        religion = st.selectbox("Which religion do you practice?", ["Agnosticism","Atheism","Christianity","Catholicism","Judaism","Buddhism","Islam","Hinduism","Unknown","others"])
+        essay3 = st.text_input("The first thing people usually notice about me")
+        essay4 = st.text_input("Favourite books, Movies, Show,Music, Food")
+        essay5 = st.text_input("The 6 things that I could never do without")
+        essay6 = st.text_input("I spend a lot of time thinking about")
+        essay7 = st.text_input("On a typical Friday night I am")
+        essay8 = st.text_input("The most private thing I am willing to admit")
+    with col2:
+        smokes = st.selectbox("Do you smoke?", ["Yes","No"])
+        drink = st.selectbox("Do you drink alcohol?", ["Yes","No"])
+        diet = st.selectbox("What kind of diet do you follow?", ["Anything","Vegan","Vegetarian","Halal","Kosher","other"])
+        speaks = st.text_input("Which language do you speak other than English?")
+        sign = st.selectbox("What is your astrological sign?", ["Aries","Taurus","Gemini","Cancer","Leo","Virgo","Libra","Scorpion","Sagittarius","Capricorn","Aquarius","Pisces"])
+        offspring = st.selectbox("What is your opinion on having children?", ["Wants Kids","Does not want kids","Has kid","Unknown"])
+        drugs = st.selectbox("Do you consume drugs?", ["Yes","No"])
+        height = st.number_input("What is your height? \n (In inches)",30,100)
+        income = st.number_input("What is your annual income?",0,100000)
+        pets = st.selectbox("What is your opinion on having pets?", ["Likes Cats and Dogs","Dislikes Cats and Dogs","Likes only cats","Likes only Dogs","Unknown"])
+        job = st.selectbox("What is your current profession?", ["Office/Professional","Science/Tech","Business Management","Creative"])
+        essay0 = st.text_input("My self summary")
+        essay1 = st.text_input("What I am doing with my life")
+        essay2 = st.text_input("I am really good at")
+        essay9 = st.text_input("You should message me if")
+    submit_button= st.form_submit_button(label='Submit')
+
 
 data = {"Name": name, "age": age, 
      "gender": gender, "orientation":orientation,
@@ -128,14 +124,10 @@ ok.drop(['essay0','essay1','essay2','essay3','essay4','essay5','essay6','essay7'
 
 corpus_df = ok.copy(deep=True)
 
-
-
 corpus_df['corpus'] = ok[['age', 'status', 'gender', 'orientation', 'body_type', 'diet', 'drink',
        'drugs', 'education', 'ethnicity', 'height', 'income', 'job',
        'offspring', 'pets', 'religion', 'sign', 'smokes', 'speaks', 'essay']].astype(str).agg(' '.join, axis=1)
 corpus_df = corpus_df.astype(str)
-
-
 
 corpus_df['corpus'] = corpus_df['corpus'].map(lambda x: x.replace('\n', ' '))
 corpus_df['corpus'] = corpus_df['corpus'].map(lambda x: x.replace('nan', ' '))
@@ -169,50 +161,24 @@ tfidf_vec = tfidf.fit_transform(corpus_list)
 corpus_2d.head()
 corpus_mat_sparse = csr_matrix(corpus_2d.values)
 
-pd.set_option('display.max_columns',25)
-pd.set_option('expand_frame_repr', False)
 
 #Model specification
 model_knn = NearestNeighbors(metric = 'cosine', algorithm = 'brute') # cosine takes 1-cosine( as cosine distance)
 model_knn.fit(corpus_mat_sparse)
 
 #recommendation algorithm (cosine)
-def rec(query_index):
-  distances, indices = model_knn.kneighbors(corpus_2d.iloc[query_index, :].values.reshape(1, -1), n_neighbors = 21)
-
-  for i in range(0, len(distances.flatten())):
-    if i == 0:
-        print('Recommendations for {0}: \n'.format(corpus_2d.index[query_index]))
-    else:
-        print('{0}: {1}, with distance of {2} \n '.format(i, corpus_2d.index[indices.flatten()[i]],distances.flatten()[i]))
-  for i in indices:
-    print(okc1.loc[i,:])
- 
-    
- 
+@st.cache
 def rec(query_index):
   distances, indices = model_knn.kneighbors(corpus_2d.iloc[query_index, :].values.reshape(1, -1), n_neighbors = 6)
   result= pd.DataFrame()
   for i in indices:
     result= result.append(okc1.iloc[i,:])
   result['similarity distance']= distances.flatten()
-  return result[['name',"similarity distance","age","status","orientation","body_type","ethnicity","religion","smokes","drink","diet","essay0"]]    
+  return result[['name',"similarity distance","age","status","orientation","ethnicity","religion","diet","essay0"]] 
 
 st.markdown('##')
 st.markdown('##')   
 
-if st.button("Join"):
+if submit_button:
     st.write("Your Matches are:")
     st.write(rec(0))
-    
-
-
-
-
-    
-
-         
-
-        
-
-
