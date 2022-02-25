08f11047fc9e6bb9f83b5e967f82de696dc7f955
burumsak <89971436+burumsak@users.noreply.github.com>
2022-02-25 13:35:54 +0530

    Update infinity.py

--- infinity.py
+++ infinity.py
@@ -29,7 +29,7 @@ st.set_page_config(
     layout="wide"
 )
 
-okc = pd.read_excel("Copy of User Details_Faf.xlsx")
+okc = pd.read_excel("User Details_Faf.xlsx")
 
 #st.title("Infinity")
 st.image("images/Infinity.png",use_column_width = True)
